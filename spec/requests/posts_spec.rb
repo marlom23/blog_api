efd1f570ed72be5aea1458128a1e37680d5797bd
Martin Lopez <martin.lopezmoguel@gmail.com>
2021-11-03 18:54:38 -0600

    Implementacion del metodo show y list,continuando con la metodologia TDD

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,31 +1,33 @@
 require "rails_helper"
+require "byebug"
+
 
 RSpec.describe 'Posts', type: :request do
 
-  describe 'GET /post' do
-    before { get '/post' }
-
+  describe 'GET /posts' do
+    before { get '/posts' }
     it 'should return OK' do
       payload = JSON.parse(response.body)
-      expect(payload).not_to be_empty
+      expect(payload).to be_empty
       expect(response).to have_http_status(200)
     end
   end
   describe 'with data in the DB' do
-    before { get '/post' }
-    let(:posts) {create_list(:post, 10, published: true) }
+    let!(:posts) {create_list(:post, 10, published: true) }
+    before { get '/posts' }
     it 'should return all the published posts' do 
+      
       payload = JSON.parse(response.body)
       expect(payload.size).to eq(posts.size)
       expect(response).to have_http_status(200)
     end
   end
 
-  describe 'GET/post/{id}' do
-    let(:post) { create(:post) }
+  describe 'GET/posts/{id}' do
+    let!(:post) { create(:post) }
 
-    it 'should return a post' do
-      get "/post/#{post.id}"
+    it 'should return a posts' do
+      get "/posts/#{post.id}"
       payload = JSON.parse(response.body)
       expect(payload).to_not be_empty 
       expect(payload['id']).to eq(post.id)
