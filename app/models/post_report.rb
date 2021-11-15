22489f31ba7716a063faf30721f43818c234a877
Martin Lopez <martin.lopezmoguel@gmail.com>
2021-11-15 17:08:06 -0600

    Generando reportes para procesar con ActiveJob

---
+++ app/models/post_report.rb
@@ -0,0 +1,24 @@
+class PostReport < Struct.new(:word_count, :word_histogram)
+  def self.generate(post)
+    PostReport.new(
+      post.content.split.map {|word| word.gsub(/\W/,'') }.count
+
+      calc_histogram(post)
+    )
+
+
+    private
+
+    def self.calc_histogram(post)
+      (post
+        .content
+        .split
+        .map {|word| word.gsub(/\W/,'') }
+        .map (&:downcase)
+        .group_by { |word| word }
+        .transform_values(&:size) )
+    end
+
+
+  end
+end
